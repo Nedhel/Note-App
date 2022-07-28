02028974068ed2bf7cb83f8e1b1dce057bb20c1e
Carlos Zalpa <nedhel.90@gmail.com>
2022-07-28 14:07:11 -0500

    save and recovery notes

--- notes.js
+++ notes.js
@@ -1,9 +1,42 @@
+function initial() {
+    const strJsonNotes =
+        '[{"id":"1","text":"Nueva nota de texto, nota 1"},{"id":"2","text":"Nota 2 solo para prueba"}]';
+    if (strJsonNotes != "") {
+        const arrayNotes = JSON.parse(strJsonNotes);
+        for (const iterator of arrayNotes) {
+            let newDiv = document.createElement("div");
+            let newButton = document.createElement("button");
+            let newTextArea = document.createElement("textarea");
+            let parent = document.getElementById("content");
+            parent.appendChild(newDiv);
+            newDiv.id = iterator.id;
+            newDiv.appendChild(newButton);
+            newButton.className = "del-itself";
+            newButton.innerText = "-";
+            newButton.addEventListener("click", () => {
+                deleteElement(iterator.id);
+            });
+            newDiv.appendChild(newTextArea);
+            newTextArea.name = iterator.id;
+            newTextArea.cols = 12;
+            newTextArea.rows = 15;
+            newTextArea.placeholder = "New Note";
+            newTextArea.value = iterator.text;
+        }
+    }
+}
+
 function addNote() {
     let newDiv = document.createElement("div");
     let newButton = document.createElement("button");
     let newTextArea = document.createElement("textarea");
-    let number =
-        Number(document.getElementById("content").lastElementChild.id) + 1;
+    let number = 0;
+    if (document.getElementById("content").lastElementChild.id == "menu") {
+        number = 1;
+    } else {
+        number =
+            Number(document.getElementById("content").lastElementChild.id) + 1;
+    }
     let parent = document.getElementById("content");
 
     parent.appendChild(newDiv);
@@ -11,6 +44,9 @@ function addNote() {
     newDiv.appendChild(newButton);
     newButton.className = "del-itself";
     newButton.innerText = "-";
+    newButton.addEventListener("click", () => {
+        deleteElement(number);
+    });
     newDiv.appendChild(newTextArea);
     newTextArea.name = number;
     newTextArea.cols = 12;
@@ -32,7 +68,22 @@ function visible() {
         }
     }
 }
+function save() {
+    const notes = document.getElementById("content").children;
+    const jsonArray = [];
+    let jump = 0;
+    for (const iterator of notes) {
+        if (jump >= 2) {
+            jsonArray.push({ id: iterator.id, text: iterator.lastChild.value });
+        }
+        jump++;
+    }
+    const fileJson = JSON.stringify(jsonArray);
+    console.log(fileJson);
+}
 window.addEventListener("load", () => {
+    initial();
     document.getElementById("delete").addEventListener("click", visible);
     document.getElementById("add").addEventListener("click", addNote);
+    document.getElementById("save").addEventListener("click", save);
 });
